61516bfe1f7d0caac51ba029290cf9cf485c45e3
c-c-c-c <4c.mura@gmail.com>
2018-11-04 19:13:07 +0900

    うーん、なぜかMainSceneを触ってしまったので、はやめにマージします。。

---
+++ Assets/Individuals/_yoshimura/FlipperManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperManager : MonoBehaviour {
+
+    public GameObject rightFlipper;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rRigidBody = rightFlipper.GetComponent<Rigidbody>();
+        print(rRigidBody);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

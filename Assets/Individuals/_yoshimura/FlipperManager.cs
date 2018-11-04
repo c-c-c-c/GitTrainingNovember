b24620bcb136f6b1d830e8c67e57355f131d7666
c-c-c-c <4c.mura@gmail.com>
2018-11-04 21:12:24 +0900

    格闘の足跡。。。

--- Assets/Individuals/_yoshimura/FlipperManager.cs
+++ Assets/Individuals/_yoshimura/FlipperManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FlipperManager : MonoBehaviour {
 
     public GameObject rightFlipper;
+    Vector3 normVec;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Rigidbody rRigidBody = rightFlipper.GetComponent<Rigidbody>();
         print(rRigidBody);
         //rRigidBody.centerOfMass = new Vector3(-2f, 0.4f, 1.2f);
@@ -20,7 +21,7 @@ public class FlipperManager : MonoBehaviour {
         //}
         Vector3 a = new Vector3(1, 0, 0);
         Vector3 b = new Vector3(0, 0.5f, Mathf.Sqrt(3));
-        Vector3 normVec = Vector3.Cross(a ,b);
+        normVec = Vector3.Cross(a, b) + new Vector3(-0, 0, 0);
         print(normVec);
 
     }
@@ -28,10 +29,13 @@ public class FlipperManager : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         rightFlipper.transform.RotateAround(
-            //new Vector3(0f, 0f, 0f),
-            //new Vector3(-2f, 0.4f, 1.2f),
             new Vector3(-2f, 0.4f, -1.2f),
-            transform.up, 40 * Time.deltaTime);
+            //new Vector3(-4f, 0.8f, -2.4f),
+            //new Vector3(0, 1.7f, -1f),
+            new Vector3(0, 1.7f, 1f),
+            //normVec,
+            //normVec,
+            40 * Time.deltaTime);
 
     }
 }

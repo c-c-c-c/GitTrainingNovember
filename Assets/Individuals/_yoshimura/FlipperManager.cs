14690c38a7fe93e3a5e49468e2ced569e4b6843a
c-c-c-c <4c.mura@gmail.com>
2018-11-04 21:47:17 +0900

    右フリップの動きを暫定で完成。

--- Assets/Individuals/_yoshimura/FlipperManager.cs
+++ Assets/Individuals/_yoshimura/FlipperManager.cs
@@ -5,30 +5,52 @@ using UnityEngine;
 public class FlipperManager : MonoBehaviour {
 
     public GameObject rightFlipper;
-    Vector3 normVec;
+    Vector3 rotateCenter = new Vector3(-2f, 0.4f, -1.2f); //回転の中心位置
+    Vector3 rotateAxis = new Vector3(0, 1.7f, 1f); //回転の軸
     int angleCount = 0;
-    public float flipSpeed = 200;
+    public float flipSpeed = 500;
 
     // Use this for initialization
     void Start () {
         Rigidbody rRigidBody = rightFlipper.GetComponent<Rigidbody>();
-        print(rRigidBody);
-        GameObject obj = GameObject.Find("__CubeMeyasu");
-
     }
 
     // Update is called once per frame
     void Update () {
 
         if ( Input.GetKey( KeyCode.RightArrow ) ) {
-            rightFlipper.transform.RotateAround(
+
+            if (angleCount <= 12)
+            {
+                rightFlipper.transform.RotateAround(
+                    new Vector3(-2f, 0.4f, -1.2f),
+                    new Vector3(0, 1.7f, 1f),
+                    flipSpeed * Time.deltaTime );
+
+                angleCount++;
+                //print(angleCount);
+            } else
+            {
+               // 特定の位置に戻す 
+            }
+
+
+        } else
+        {
+            if ( 0 < angleCount )
+            {
+                rightFlipper.transform.RotateAround(
                 new Vector3(-2f, 0.4f, -1.2f),
                 new Vector3(0, 1.7f, 1f),
-                flipSpeed * Time.deltaTime);
+                -flipSpeed * Time.deltaTime);
+                angleCount--;
 
-            angleCount++;
-            print(angleCount);
+            } else
+            {
+                //特定の位置に、、
+            }
+
         }
-        print(rightFlipper.transform.rotation);
+
     }
 }

a90691b75c78cc733499f50fefe635caf50eec98
c-c-c-c <4c.mura@gmail.com>
2018-11-04 21:20:26 +0900

    右のフリップが動くように。（無制限に動く）

--- Assets/Individuals/_yoshimura/FlipperManager.cs
+++ Assets/Individuals/_yoshimura/FlipperManager.cs
@@ -6,36 +6,29 @@ public class FlipperManager : MonoBehaviour {
 
     public GameObject rightFlipper;
     Vector3 normVec;
+    int angleCount = 0;
+    public float flipSpeed = 200;
 
     // Use this for initialization
     void Start () {
         Rigidbody rRigidBody = rightFlipper.GetComponent<Rigidbody>();
         print(rRigidBody);
-        //rRigidBody.centerOfMass = new Vector3(-2f, 0.4f, 1.2f);
         GameObject obj = GameObject.Find("__CubeMeyasu");
-        //MeshFilter mf = obj.GetComponent<MeshFilter>();
-        //Vector3[] nor = mf.mesh.normals;
-        //foreach ( var i  in nor )
-        //{
-        //    print(i);
-        //}
-        Vector3 a = new Vector3(1, 0, 0);
-        Vector3 b = new Vector3(0, 0.5f, Mathf.Sqrt(3));
-        normVec = Vector3.Cross(a, b) + new Vector3(-0, 0, 0);
-        print(normVec);
 
     }
 
     // Update is called once per frame
     void Update () {
-        rightFlipper.transform.RotateAround(
-            new Vector3(-2f, 0.4f, -1.2f),
-            //new Vector3(-4f, 0.8f, -2.4f),
-            //new Vector3(0, 1.7f, -1f),
-            new Vector3(0, 1.7f, 1f),
-            //normVec,
-            //normVec,
-            40 * Time.deltaTime);
 
+        if ( Input.GetKey( KeyCode.RightArrow ) ) {
+            rightFlipper.transform.RotateAround(
+                new Vector3(-2f, 0.4f, -1.2f),
+                new Vector3(0, 1.7f, 1f),
+                flipSpeed * Time.deltaTime);
+
+            angleCount++;
+            print(angleCount);
+        }
+        print(rightFlipper.transform.rotation);
     }
 }

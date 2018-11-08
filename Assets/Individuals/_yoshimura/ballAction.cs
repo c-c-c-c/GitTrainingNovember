30fd64b65e1fa02625b5ce878c79286768a98c3f
c-c-c-c <4c.mura@gmail.com>
2018-11-09 07:42:04 +0900

    得点が追加されるように。

---
+++ Assets/Individuals/_yoshimura/ballAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ballAction : MonoBehaviour {
+
+    GameManger gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManger>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        print(other.gameObject.tag);
+        if ( other.gameObject.tag == "kugi" ) {
+            gameManager.ScoreUpdate(30);
+        }
+    }
+}
